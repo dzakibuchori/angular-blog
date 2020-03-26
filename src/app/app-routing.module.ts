969c79ec4ed3df661bbbb87591b5fd72e4b9d1f6
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-26 16:45:26 +0700

    initiate path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ArticleComponent} from './article/article.component';
+import {Error404Component} from './error404/error404.component';
+import {LoginComponent} from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ArticleComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    component: Error404Component
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
