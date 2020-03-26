16197738275dfbad13b373eb051021783644e49b
Dzaki Buchori <dzakibuchori@gmail.com>
2020-03-26 16:48:54 +0700

    import components and module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { SingleArticleComponent } from './single-article/single-article.component';
+import { LoginComponent } from './login/login.component';
+import { Error404Component } from './error404/error404.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    SingleArticleComponent,
+    LoginComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AdminModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
